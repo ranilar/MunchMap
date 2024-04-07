c17981111b91ff2bcb4322d5e8fa8b60a26463be
Ilari Ranin <raninilari@gmail.com>
2024-04-07 22:09:14 +0300

    Initialized the map and some text on the front page. Map has no features as of yet, but are coming very soon.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template, redirect, session, request
 from os import getenv
+import googlemaps
 
 app = Flask(__name__)
 
@@ -11,6 +12,11 @@ def index():
 
 #login
 2
-@app.route("/login")
-def login():
-    return render_template("login.html")
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/map")
+def maps():
+    return render_template("map.html")
+
---
+++ static/script.js
@@ -0,0 +1,7 @@
+function initMap(){
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: {lat: 60.16697266828053, lng: 24.943136300431043},
+        zoom: 16,
+        mapId:"7d132948a44ae00a"
+    });
+}
